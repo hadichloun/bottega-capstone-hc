ded848a035290bdd989c98b2eb776374138104c6
hadichloun <hadi_960@hotmail.com>
2021-01-23 16:50:22 -0600

    intital button components and run router inside

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
          <nav className="navbar">
              <div className="navbar-container">
                  <Link to="/" className="navbar-logo">
-                     Bottega's Grill <i class="fas fa-fire-alt"></i><i class="fas fa-fire"></i>
+                     Bottega's Grill <i class="fas fa-fire-alt"></i><i class="fas fa-fire"></i><i class="fas fa-circle-notch"></i><i class="fas fa-spinner"></i>
                  </Link>
                  <div className='menu-icon' onClick={handleClick}>
                      <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -20,6 +20,21 @@ function Navbar() {
                      <li className='nav-item'>
                          <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                              Home
+                         </Link>
+                     </li>
+                     <li className='nav-item'>
+                         <Link to='/Menu' className='nav-links' onClick={closeMobileMenu}>
+                             Menu
+                         </Link>
+                     </li>
+                     <li className='nav-item'>
+                         <Link to='/About Us' className='nav-links' onClick={closeMobileMenu}>
+                             About Us
+                         </Link>
+                     </li>
+                     <li className='nav-item'>
+                         <Link to='/reservation' className='nav-links-mobile' onClick={closeMobileMenu}>
+                             reservation
                          </Link>
                      </li>
                    </ul>
