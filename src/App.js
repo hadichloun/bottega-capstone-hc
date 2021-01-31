ae0d5bd69c10c819f4051c9732c9083dbe64a537
hadichloun <hadi_960@hotmail.com>
2021-01-31 08:42:23 -0600

    adds hover in gfooter

--- src/App.js
+++ src/App.js
@@ -4,3 +4,28 @@ import { BrowserRouter as Router, Switch, Route}  from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Menu from './components/pages/Menu';
+import AboutUs from './components/pages/AboutUs';
+import Reservations from './components/pages/Reservations';
+// import Chef from './components/pages/Chef';
+
+
+
+
+function App() {
+  return (
+    <>
+    <Router>
+       <Navbar />
+       <Switch>
+         <Route path='/' exact component= {Home} />
+         <Route path='/menu' component={Menu} />
+         <Route path='/about-us' component={AboutUs} />
+         <Route path='/reservations' component={Reservations} />
+         {/* <Route path='/chef' component={Chef} /> */}
+         </Switch>
+        </Router>
+    </>
+  );
+}
+
+export default App;
