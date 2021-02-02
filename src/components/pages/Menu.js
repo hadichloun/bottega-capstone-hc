a19b14796d863a2aa3e7f70ac31650e0f4d54b0e
hadichloun <hadi_960@hotmail.com>
2021-02-01 20:26:50 -0600

    fixes menu route and styles

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../../App.css';
-import { Container, Row, Col } from 'reactstrap'
+
 
 export default function Menu() {
     return <h1 className='menu'>Menu</h1>
 }
+
+
+
